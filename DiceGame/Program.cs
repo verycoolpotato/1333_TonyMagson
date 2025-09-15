36f98af21a494d402a2227523aac35676637f995
verycoolpotato <100438125+verycoolpotato@users.noreply.github.com>
2025-09-15 15:09:06 -0700

    Dice Game

---
+++ DiceGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using DiceGame.Scripts;
+
+namespace DiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameManager manager = new GameManager();
+            manager.PlayGame();
+        }
+    }
+}
---
+++ DiceGame/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame.Scripts
+{
+    internal class GameManager
+    {
+        private int score = 0;
+        public void PlayGame()
+        {
+            Console.WriteLine("Time to Die!");
+
+            OnPointAquired();
+        }
+
+        public void OnPointAquired()
+        {
+            score++;
+            string gainedMessage = "You Gained a Point!";
+            Console.WriteLine(gainedMessage);
+        }
+    }
+}
